2714f89c0c49b0652b3c4ad85e854a437ab0747f
odalydarragh <95000799+odalydarragh@users.noreply.github.com>
2022-06-10 15:52:31 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner askNum = new Scanner(System.in);
+        System.out.print("Please enter your number here: ");
+        int userNum = askNum.nextInt();
+
+        if (userNum % 2 == 0)
+            System.out.println("Buzz");
+
+        else if (userNum % 5 == 0)
+            System.out.println("Fizz");
+
+        else ((userNum % 5) != 0)
+            System.out.println(userNum);
+
+
+    }
+}
